e2cefdbeb7ca5b52dab6442b519403d272e9db51
Anwesh43 <anweshthecool0@gmail.com>
2023-03-09 19:16:28 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 
 export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) => {
     const [scale, setScale] = useState(0)
@@ -32,7 +32,7 @@ export const useDimension = () => {
             setH(window.innerHeight)
             return () => {
                 window.onresize = () => {
-                    
+
                 }
             }
         }
@@ -40,5 +40,23 @@ export const useDimension = () => {
     return {
         w, 
         h
+    }
+}
+
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const size = Math.min(w, h) / 12
+    return {
+        buttonStyle() : CSSProperties {
+            return {
+                position,
+                left: `${w / 2 - size / 2}px`,
+                top: `${(h / 2 - size / 6) * (1 + sinify(scale))}px`,
+                width: `${size}px`,
+                height: `${size / 3}px`
+            }
+        }
     }
 }
